190cbd2d249be258d0760d838a1c412d31b18089
Jimmy Easter <jmeaster1@gmail.com>
2017-06-18 21:56:39 -0400

    Add exercise 28

---
+++ SafeSquareRoot.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SafeSquareRoot {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		double x, y;
+		String entry;
+		
+		System.out.print("Are you ready?!?");
+		entry = keyboard.next();
+		
+		while (! entry.equals("yes") ) {
+			System.out.println("Are you ready?");
+			entry = keyboard.next();
+		}
+		
+		System.out.print("Give me a number, and I'll find it's square root.");
+		System.out.print("(No negatives, please.)");
+		x = keyboard.nextDouble();
+		
+		while ( x < 0 ) {
+			System.out.println("I won't take the square root of a negative.");
+			System.out.print("\nNew number: ");
+			x = keyboard.nextDouble();
+		}
+		
+		y = Math.sqrt(x);
+		
+		System.out.println("The square root of " + x + " is " + y + ".");
+	}
+}
